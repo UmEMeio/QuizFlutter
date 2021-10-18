7dc1d8882b28f42f82599db59f87ccee474583a4
Gabriel Costa <52284050+UmEMeio@users.noreply.github.com>
2021-10-18 16:29:40 -0300

    adicionando style na questao

--- lib/question.dart
+++ lib/question.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  String Questiontexto;
+  final String Questiontexto;
   Question(this.Questiontexto);
   @override
   Widget build(BuildContext context) {
-    return Text(Questiontexto);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        Questiontexto,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
